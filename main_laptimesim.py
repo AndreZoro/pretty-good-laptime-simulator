8b5cf67f3499fc0031d49e5e36b16f89422828aa
MantiumAndre <a.zimmer@mantiumcae.com>
2026-01-11 19:15:57 +0100

    Update main lap time simulation script

--- main_laptimesim.py
+++ main_laptimesim.py
@@ -3,7 +3,6 @@ import time
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import pkg_resources
 import pickle
 
 """
@@ -26,37 +25,40 @@ Input tracks must be unclosed, i.e. last point != first point!
 # MAIN FUNCTION --------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-def main(track_opts: dict,
-         solver_opts: dict,
-         driver_opts: dict,
-         sa_opts: dict,
-         debug_opts: dict) -> laptimesim.src.lap.Lap:
 
-    # ------------------------------------------------------------------------------------------------------------------
-    # CHECK PYTHON DEPENDENCIES ----------------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------------------------------------------
+def main(
+    track_opts: dict,
+    solver_opts: dict,
+    driver_opts: dict,
+    sa_opts: dict,
+    debug_opts: dict,
+) -> laptimesim.src.lap.Lap:
+    # # ------------------------------------------------------------------------------------------------------------------
+    # # CHECK PYTHON DEPENDENCIES ----------------------------------------------------------------------------------------
+    # # ------------------------------------------------------------------------------------------------------------------
 
-    # get repo path
-    repo_path = os.path.dirname(os.path.abspath(__file__))
+    # # get repo path
+    # repo_path = os.path.dirname(os.path.abspath(__file__))
 
-    # read dependencies from requirements.txt
-    requirements_path = os.path.join(repo_path, 'requirements.txt')
-    dependencies = []
+    # # read dependencies from requirements.txt
+    # requirements_path = os.path.join(repo_path, 'requirements.txt')
+    # dependencies = []
 
-    with open(requirements_path, 'r') as fh:
-        line = fh.readline()
+    # with open(requirements_path, 'r') as fh:
+    #     line = fh.readline()
 
-        while line:
-            dependencies.append(line.rstrip())
-            line = fh.readline()
+    #     while line:
+    #         dependencies.append(line.rstrip())
+    #         line = fh.readline()
 
-    # check dependencies
-    pkg_resources.require(dependencies)
+    # # check dependencies
+    # pkg_resources.require(dependencies)
 
     # ------------------------------------------------------------------------------------------------------------------
     # INITIALIZATION ---------------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
+    repo_path = os.path.dirname(os.path.abspath(__file__))
     output_path = os.path.join(repo_path, "laptimesim", "output")
 
     output_path_velprofile = os.path.join(output_path, "velprofile")
@@ -73,15 +75,29 @@ def main(track_opts: dict,
     # CREATE TRACK INSTANCE --------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
-    parfilepath = os.path.join(repo_path, "laptimesim", "input", "tracks", "track_pars.ini")
+    parfilepath = os.path.join(
+        repo_path, "laptimesim", "input", "tracks", "track_pars.ini"
+    )
 
     if not track_opts["use_pit"]:  # normal case
-        trackfilepath = os.path.join(repo_path, "laptimesim", "input", "tracks", "racelines",
-                                     track_opts["trackname"] + ".csv")
+        trackfilepath = os.path.join(
+            repo_path,
+            "laptimesim",
+            "input",
+            "tracks",
+            "racelines",
+            track_opts["trackname"] + ".csv",
+        )
 
     else:  # case pit
-        trackfilepath = os.path.join(repo_path, "laptimesim", "input", "tracks", "racelines",
-                                     track_opts["trackname"] + "_pit.csv")
+        trackfilepath = os.path.join(
+            repo_path,
+            "laptimesim",
+            "input",
+            "tracks",
+            "racelines",
+            track_opts["trackname"] + "_pit.csv",
+        )
 
     # set velocity limit
     if driver_opts["vel_lim_glob"] is not None:
@@ -92,13 +108,15 @@ def main(track_opts: dict,
         vel_lim_glob = np.inf
 
     # create instance
-    track = laptimesim.src.track.Track(pars_track=track_opts,
-                                       parfilepath=parfilepath,
-                                       trackfilepath=trackfilepath,
-                                       vel_lim_glob=vel_lim_glob,
-                                       yellow_s1=driver_opts["yellow_s1"],
-                                       yellow_s2=driver_opts["yellow_s2"],
-                                       yellow_s3=driver_opts["yellow_s3"])
+    track = laptimesim.src.track.Track(
+        pars_track=track_opts,
+        parfilepath=parfilepath,
+        trackfilepath=trackfilepath,
+        vel_lim_glob=vel_lim_glob,
+        yellow_s1=driver_opts["yellow_s1"],
+        yellow_s2=driver_opts["yellow_s2"],
+        yellow_s3=driver_opts["yellow_s3"],
+    )
 
     # debug plot
     if debug_opts["use_debug_plots"]:
@@ -124,15 +142,27 @@ def main(track_opts: dict,
     # CREATE CAR INSTANCE ----------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
-    parfilepath = os.path.join(repo_path, "laptimesim", "input", "vehicles", solver_opts["vehicle"])
-
-    # create instance
-    if solver_opts["series"] == "F1":
-        car = laptimesim.src.car_hybrid.CarHybrid(parfilepath=parfilepath)
-    elif solver_opts["series"] == "FE":
-        car = laptimesim.src.car_electric.CarElectric(parfilepath=parfilepath)
+    # create instance - use custom params if provided, otherwise load from file
+    if solver_opts.get("custom_vehicle_pars") is not None:
+        # Use custom vehicle parameters
+        pars_veh = solver_opts["custom_vehicle_pars"]
+        if solver_opts["series"] == "F1":
+            car = laptimesim.src.car_hybrid.CarHybrid(pars_veh=pars_veh)
+        elif solver_opts["series"] == "FE":
+            car = laptimesim.src.car_electric.CarElectric(pars_veh=pars_veh)
+        else:
+            raise IOError("Unknown racing series!")
     else:
-        raise IOError("Unknown racing series!")
+        # Load from file
+        parfilepath = os.path.join(
+            repo_path, "laptimesim", "input", "vehicles", solver_opts["vehicle"]
+        )
+        if solver_opts["series"] == "F1":
+            car = laptimesim.src.car_hybrid.CarHybrid(parfilepath=parfilepath)
+        elif solver_opts["series"] == "FE":
+            car = laptimesim.src.car_electric.CarElectric(parfilepath=parfilepath)
+        else:
+            raise IOError("Unknown racing series!")
 
     # debug plot
     if debug_opts["use_debug_plots"]:
@@ -148,19 +178,17 @@ def main(track_opts: dict,
     # ------------------------------------------------------------------------------------------------------------------
 
     # create instance
-    driver = laptimesim.src.driver.Driver(carobj=car,
-                                          pars_driver=driver_opts,
-                                          trackobj=track,
-                                          stepsize=track.stepsize)
+    driver = laptimesim.src.driver.Driver(
+        carobj=car, pars_driver=driver_opts, trackobj=track, stepsize=track.stepsize
+    )
 
     # ------------------------------------------------------------------------------------------------------------------
     # CREATE LAP INSTANCE ----------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
-    lap = laptimesim.src.lap.Lap(driverobj=driver,
-                                 trackobj=track,
-                                 pars_solver=solver_opts,
-                                 debug_opts=debug_opts)
+    lap = laptimesim.src.lap.Lap(
+        driverobj=driver, trackobj=track, pars_solver=solver_opts, debug_opts=debug_opts
+    )
 
     # ------------------------------------------------------------------------------------------------------------------
     # CALL SOLVER ------------------------------------------------------------------------------------------------------
@@ -201,7 +229,9 @@ def main(track_opts: dict,
         lap.pars_solver["print_debug"] = False
 
         # create parameter ranges
-        sa_range_1 = np.linspace(sa_opts["range_1"][0], sa_opts["range_1"][1], sa_opts["range_1"][2])
+        sa_range_1 = np.linspace(
+            sa_opts["range_1"][0], sa_opts["range_1"][1], sa_opts["range_1"][2]
+        )
         # if sa_opts["range_2"] is not None:
         #     sa_range_2 = np.linspace(sa_opts["range_2"][0], sa_opts["range_2"][1], sa_opts["range_2"][2])
 
@@ -241,10 +271,14 @@ def main(track_opts: dict,
         print("Solver runtime: %.2f s" % (time.perf_counter() - t_start))
         print("-" * 50)
         print("Lap time: %.3f s" % lap.t_cl[-1])
-        print("S1: %.3f s  |  S2: %.3f s  |  S3: %.3f s" %
-              (lap.t_cl[track.zone_inds["s12"]],
-               lap.t_cl[track.zone_inds["s23"]] - lap.t_cl[track.zone_inds["s12"]],
-               lap.t_cl[-1] - lap.t_cl[track.zone_inds["s23"]]))
+        print(
+            "S1: %.3f s  |  S2: %.3f s  |  S3: %.3f s"
+            % (
+                lap.t_cl[track.zone_inds["s12"]],
+                lap.t_cl[track.zone_inds["s23"]] - lap.t_cl[track.zone_inds["s12"]],
+                lap.t_cl[-1] - lap.t_cl[track.zone_inds["s23"]],
+            )
+        )
         print("-" * 50)
         v_tmp = lap.vel_cl[0] * 3.6
         print("Start velocity: %.1f km/h" % v_tmp)
@@ -253,14 +287,19 @@ def main(track_opts: dict,
         v_tmp = (lap.vel_cl[0] - lap.vel_cl[-1]) * 3.6
         print("Delta: %.1f km/h" % v_tmp)
         print("-" * 50)
-        print("Consumption: %.2f kg/lap | %.2f kJ/lap" % (lap.fuel_cons_cl[-1], lap.e_cons_cl[-1] / 1000.0))
+        print(
+            "Consumption: %.2f kg/lap | %.2f kJ/lap"
+            % (lap.fuel_cons_cl[-1], lap.e_cons_cl[-1] / 1000.0)
+        )
         # [J] -> [kJ]
         print("-" * 50)
 
     elif debug_opts["use_print_result"]:
         print("-" * 50)
         print("Forward/Backward Plus Solver")
-        print("Runtime for sensitivity analysis: %.2f s" % (time.perf_counter() - t_start))
+        print(
+            "Runtime for sensitivity analysis: %.2f s" % (time.perf_counter() - t_start)
+        )
         print("-" * 50)
 
         if sa_opts["sa_type"] == "mass":
@@ -268,8 +307,14 @@ def main(track_opts: dict,
             t_lap_diff = sa_t_lap[-1] - sa_t_lap[0]
             fuel_cons_diff = sa_fuel_cons[-1] - sa_fuel_cons[0]
 
-            print("Average sensitivity of lap time to mass: %.3f s/kg" % (t_lap_diff / m_diff))
-            print("Average sensitivity of fuel consumption to mass: %.5f kg/kg" % (fuel_cons_diff / m_diff))
+            print(
+                "Average sensitivity of lap time to mass: %.3f s/kg"
+                % (t_lap_diff / m_diff)
+            )
+            print(
+                "Average sensitivity of fuel consumption to mass: %.5f kg/kg"
+                % (fuel_cons_diff / m_diff)
+            )
             print("-" * 50)
 
         else:
@@ -277,7 +322,9 @@ def main(track_opts: dict,
             # TODO: implementation of COG and aero variation missing
 
     # write velocity profile output
-    output_path = os.path.join(output_path_velprofile, "velprofile_" + track_opts["trackname"].lower() + ".csv")
+    output_path = os.path.join(
+        output_path_velprofile, "velprofile_" + track_opts["trackname"].lower() + ".csv"
+    )
 
     tmp_data = np.column_stack((lap.trackobj.dists_cl[:-1], lap.vel_cl[:-1]))
 
@@ -324,9 +371,11 @@ def main(track_opts: dict,
     # ------------------------------------------------------------------------------------------------------------------
 
     # pickle lap object for possible CI testing
-    result_objects_file_path = os.path.join(output_path_testobjects,
-                                            "testobj_laptimesim_" + track_opts["trackname"] + ".pkl")
-    with open(result_objects_file_path, 'wb') as fh:
+    result_objects_file_path = os.path.join(
+        output_path_testobjects,
+        "testobj_laptimesim_" + track_opts["trackname"] + ".pkl",
+    )
+    with open(result_objects_file_path, "wb") as fh:
         pickle.dump(lap, fh)
 
     return lap  # return required in case of CI testing
@@ -336,8 +385,7 @@ def main(track_opts: dict,
 # MAIN FUNCTION CALL ---------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # ------------------------------------------------------------------------------------------------------------------
     # USER INPUT -------------------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
@@ -358,14 +406,16 @@ if __name__ == '__main__':
     # use_drs2:             DRS zone 2 switch
     # use_pit:              activate pit stop (requires _pit track file!)
 
-    track_opts_ = {"trackname": "Shanghai",
-                   "flip_track": False,
-                   "mu_weather": 1.0,
-                   "interp_stepsize_des": 5.0,
-                   "curv_filt_width": 10.0,
-                   "use_drs1": True,
-                   "use_drs2": True,
-                   "use_pit": False}
+    track_opts_ = {
+        "trackname": "Shanghai",
+        "flip_track": False,
+        "mu_weather": 1.0,
+        "interp_stepsize_des": 5.0,
+        "curv_filt_width": 10.0,
+        "use_drs1": True,
+        "use_drs2": True,
+        "use_pit": False,
+    }
 
     # solver options ---------------------------------------------------------------------------------------------------
     # vehicle:                  vehicle parameter file
@@ -377,13 +427,15 @@ if __name__ == '__main__':
     # max_no_em_iters:          maximum number of iterations for EM recalculation
     # es_diff_max:              [J] stop criterion -> maximum difference between two solver runs
 
-    solver_opts_ = {"vehicle": "F1_Shanghai.ini",
-                    "series": "F1",
-                    "limit_braking_weak_side": 'FA',
-                    "v_start": 100.0 / 3.6,
-                    "find_v_start": True,
-                    "max_no_em_iters": 5,
-                    "es_diff_max": 1.0}
+    solver_opts_ = {
+        "vehicle": "F1_Shanghai.ini",
+        "series": "F1",
+        "limit_braking_weak_side": "FA",
+        "v_start": 100.0 / 3.6,
+        "find_v_start": True,
+        "max_no_em_iters": 5,
+        "es_diff_max": 1.0,
+    }
 
     # driver options ---------------------------------------------------------------------------------------------------
     # vel_subtr_corner: [m/s] velocity subtracted from max. cornering vel. since drivers will not hit the maximum
@@ -401,17 +453,19 @@ if __name__ == '__main__':
     # use_lift_coast:   switch to turn lift and coast on/off
     # lift_coast_dist:  [m] lift and coast before braking point
 
-    driver_opts_ = {"vel_subtr_corner": 0.5,
-                    "vel_lim_glob": None,
-                    "yellow_s1": False,
-                    "yellow_s2": False,
-                    "yellow_s3": False,
-                    "yellow_throttle": 0.3,
-                    "initial_energy": 4.0e6,
-                    "em_strategy": "FCFB",
-                    "use_recuperation": True,
-                    "use_lift_coast": False,
-                    "lift_coast_dist": 10.0}
+    driver_opts_ = {
+        "vel_subtr_corner": 0.5,
+        "vel_lim_glob": None,
+        "yellow_s1": False,
+        "yellow_s2": False,
+        "yellow_s3": False,
+        "yellow_throttle": 0.3,
+        "initial_energy": 4.0e6,
+        "em_strategy": "FCFB",
+        "use_recuperation": True,
+        "use_lift_coast": False,
+        "lift_coast_dist": 10.0,
+    }
 
     # sensitivity analysis options -------------------------------------------------------------------------------------
     # use_sa:   switch to deactivate sensitivity analysis
@@ -419,10 +473,12 @@ if __name__ == '__main__':
     # range_1:  range of parameter variation [start, end, number of steps]
     # range_2:  range of parameter variation [start, end, number of steps] -> CURRENTLY NOT IMPLEMENTED
 
-    sa_opts_ = {"use_sa": False,
-                "sa_type": "mass",
-                "range_1": [733.0, 833.0, 5],
-                "range_2": None}
+    sa_opts_ = {
+        "use_sa": False,
+        "sa_type": "mass",
+        "range_1": [733.0, 833.0, 5],
+        "range_2": None,
+    }
 
     # debug options ----------------------------------------------------------------------------------------------------
     # use_plot:                 plot results
@@ -431,18 +487,22 @@ if __name__ == '__main__':
     # use_print:                set if prints to console should be used or not (does not suppress hints/warnings)
     # use_print_result:         set if result should be printed to console or not
 
-    debug_opts_ = {"use_plot": False,
-                   "use_debug_plots": False,
-                   "use_plot_comparison_tph": False,
-                   "use_print": True,
-                   "use_print_result": True}
+    debug_opts_ = {
+        "use_plot": False,
+        "use_debug_plots": False,
+        "use_plot_comparison_tph": False,
+        "use_print": True,
+        "use_print_result": True,
+    }
 
     # ------------------------------------------------------------------------------------------------------------------
     # SIMULATION CALL --------------------------------------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
 
-    main(track_opts=track_opts_,
-         solver_opts=solver_opts_,
-         driver_opts=driver_opts_,
-         sa_opts=sa_opts_,
-         debug_opts=debug_opts_)
+    main(
+        track_opts=track_opts_,
+        solver_opts=solver_opts_,
+        driver_opts=driver_opts_,
+        sa_opts=sa_opts_,
+        debug_opts=debug_opts_,
+    )
